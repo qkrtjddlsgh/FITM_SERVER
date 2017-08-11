07e8603bd192af114edc1a8fb48f70114d0907c9
Park Seong Ho <glgl2131@naver.com>
2017-08-11 20:48:00 +0900

    cancel class 수정

--- routes/reserve_classes/res_classes_stom/res_cancel_classes_m.js
+++ routes/reserve_classes/res_classes_stom/res_cancel_classes_m.js
@@ -5,30 +5,60 @@ var time_table = require('../../../models/Time_Table');
 router.post('/', function(req, res){
     var recv_data = req.body;
 
+    var date = recv_data.date;
     var access_key = recv_data.access_key;
     var name = recv_data.name;
     var comments = recv_data.comments;
 
-    var query = {$pull : { "classes.$.participant" : {"name" : name, "access_key" : access_key, "comments" : comments}}};
+    var chk_data = new Object();
+    chk_data.found = 0;
 
-    time_table.update({classes : {$elemMatch : {participant : {"name" : name, "access_key" : access_key, "comments" : comments}}}}, query, function (err , result) {
-        if(err){
-            console.error(err.message);
-        }else {
+    time_table.find({date: date}, function(err, result){
+
+        for (var i = 0; i < result[0].classes.length; i++) {
+            for (var j = 0; j < result[0].classes[i].participant.length; j++) {
+                if (result[0].classes[i].participant[j].access_key == access_key) {
+                    // access_key가 들어있는 수업을 찾았을때
+                    chk_data.found = 1;
+
+                    var add_data = new Object();
+
+                    add_data.message = "Canceled Class";
+                    add_data.date = result[0].date;
+                    add_data.class_num = result[0].classes[i].class_num;
+                    add_data.access_key = result[0].classes[i].participant[j].access_key;
+
+                    var query = {$pull : { "classes.$.participant" : {"name" : name, "access_key" : access_key, "comments" : comments}}};
+
+                    time_table.update({classes : {$elemMatch : {participant : {"name" : name, "access_key" : access_key, "comments" : comments}}}}, query, function (err , result) {
+                        if(err){
+                            console.error(err.message);
+                        }
+                    });
+
+                    var res_data = new Object();
+                    // 성공적으로 수업 삭제 시
+                    res_data.code = "1270";
+                    res_data.response = add_data;
+
+                    res.send(res_data);
+                    res.end();
+                }
+            }
+        }
+
+        if(chk_data.found == 0){
             var add_data = new Object();
-            add_data.message = "Canceled Class";
-            add_data.name = recv_data.name;
-            add_data.comments = recv_data.comments;
+            add_data.message = "Not enrolled";
 
             var res_data = new Object();
-            // 성공적으로 수업 삭제 시
-            res_data.code = "1270";
+            res_data.code = "2170";
             res_data.response = add_data;
 
             res.send(res_data);
             res.end();
         }
-    })
+    });
 });
 
 module.exports = router;
