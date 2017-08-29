50e6d4198a664de50999df7434c524a39fbf92ad
Park-Ryeong-Min <somuch0321@gmail.com>
2017-08-29 19:56:06 +0900

    socket.io module 수정 중

--- chat_modules/chat_on.js
+++ chat_modules/chat_on.js
@@ -4,7 +4,7 @@ var messageLog = require('../models/message_log');
 
 io.on('connection', function (socket) {
     console.log(socket.id + " has connected. [" + Date.now() + "]");
-
+    socket.emit('connection', 'connect');
     // 데이터 패킷의 기본 형태는
     // arg1 : access_key
     // arg2 : sender
@@ -13,8 +13,9 @@ io.on('connection', function (socket) {
 
     // 방 입장 이벤트 정의
     socket.on('join_room', function (data) {
-        socket.join(data.access_key + '-room');
+        socket.join(data.room_name);
         socket.leave(socket.id); // 소켓이 생성되면, 소켓의 id 값을 가진 디폴트 룸이 생성되는데, 디폴트 룸을 나가는 명령
+        console.log(JSON.stringify(data));
 
         messageLog.find({room_name : data.room_name}, function (err, result) {
             if(err){
@@ -22,13 +23,13 @@ io.on('connection', function (socket) {
                 // DB 접속 간의 에러 발생 - 처리 코드 필요
             } else{
                 if(result.length == 0){
-                    /*
+
                     var newLog = new messageLog();
                     newLog.access_key = data.access_key;
                     newLog.room_name = data.room_name;
                     newLog.message_list = [];
                     newLog.save();
-                    */
+
                     // document 가 없으면 에러가 발생하는 상황이다.
                 } else{
                     var result_data = result[0];
@@ -50,17 +51,18 @@ io.on('connection', function (socket) {
 
     // 메시지 전달 이벤트 정의
     socket.on('send_message', function (data) {
+        console.log(JSON.stringify(data));
         var send_data = new Object();
         send_data.access_key = data.access_key;
-        send_data.sender_name = data.sender_name;
+        send_data.sender = data.sender;
         send_data.room_name = data.room_name;
         send_data.message = data.message;
-        socket.broadcast.to(data.room_name).emit('send_message', send_data);
+        console.log(JSON.stringify(send_data));
+        io.sockets.in(send_data.room_name).emit('send_message', send_data);
 
         messageLog.find({room_name : data.room_name}, function (err, result) {
             if(err){
                 console.error(err.message);
-
                 // DB 연결 에러 시
             }
             else{
@@ -68,11 +70,11 @@ io.on('connection', function (socket) {
                 messageLog.update({room_name : data.room_name}, query, function (err, result) {
                     if(err){
                         console.error(err.message);
-
                         // DB 연결 에러 시
                     }
                     else{
                         // document update 성공 시
+                        console.log(result);
                     }
                 });
             }
