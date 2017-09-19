2605a8650fb40700e355c101cd0a54b12dfb52b3
Park-Ryeong-Min <somuch0321@gmail.com>
2017-09-19 20:51:50 +0900

    boxNotification CRUD module 구현 완료

--- routes/box_notification/delete_notification.js
+++ routes/box_notification/delete_notification.js
@@ -4,6 +4,33 @@ var boxNotification = require('../../models/boxNotification');
 
 router.post('/', function (req, res) {
 
+    var recv_data = req.body;
+    var notification_idx = recv_data.notification_idx;
+
+    boxNotification.remove({notification_idx : notification_idx}, function (err, result) {
+        if(err){
+            console.error(JSON.stringify(err));
+            var send_obj = new Object();
+            send_obj.code = "5800";
+            send_obj.response = {message : "database error"};
+            res.send(send_obj);
+            res.end();
+        }else{
+            var send_obj = new Object();
+            if(result.n < 1){
+                send_obj.code = "2503";
+                send_obj.response = { message : "no document"};
+                res.send(send_obj);
+                res.end();
+            }else {
+                send_obj.code = "2500";
+                send_obj.response = result;
+                res.send(send_obj);
+                res.end();
+            }
+        }
+    });
+
 });
 
 module.exports = router;
--- routes/box_notification/update_notification.js
+++ routes/box_notification/update_notification.js
@@ -4,6 +4,31 @@ var boxNotification = require('../../models/boxNotification');
 
 router.post('/', function (req, res) {
 
+    var recv_data = req.body;
+    var notification_idx = recv_data.notification_idx;
+    var update_title = recv_data.title;
+    var update_body = recv_data.body;
+
+    var find_query = { notification_idx : notification_idx };
+    var update_query = { $set : { title : update_title, body : update_body }};
+
+    boxNotification.update(find_query, update_query, function (err, result) {
+       if(err){
+           console.error(JSON.stringify(err));
+           var send_obj = new Object();
+           send_obj.code = "5800";
+           send_obj.response = {message : "database error"};
+           res.send(send_obj);
+           res.end();
+       }else {
+           var send_obj = new Object();
+           send_obj.code = "2600";
+           send_obj.response = result;
+           res.send(send_obj);
+           res.end();
+       }
+    });
+
 });
 
 module.exports = router;
