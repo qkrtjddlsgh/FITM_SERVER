1ed953171896882aa0580d686ca9c40864913b83
park seong ho <park@parkui-MacBook-Pro.local>
2017-09-30 18:21:20 +0900

    info_item 추가 구현

---
+++ routes/small_market/market_to_mobile/info_item.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router =  express.Router();
+var smallmarket = require('../../../models/Smallmarket');
+
+router.post('/', function (req, res) {
+
+    var recv_data = req.body;
+
+    var name = recv_data.name;
+
+    smallmarket.find({name: name}, function(err, doc){
+        if(err){
+            console.error(err.message);
+        }
+        if(doc.length == 0){
+            var res_data = new Object();
+
+            res_data.code = "8888";
+            res_data.message = "Item is not exist";
+
+            res.send(res_data);
+            res.end();
+        }
+        else{
+            var add_data = new Object();
+            add_data.in_content = doc[0].in_content;
+            add_data.image_list = doc[0].image_list;
+
+            var send_data = new Object();
+            send_data.code = "9999";
+            send_data.response = add_data;
+
+            res.send(send_data);
+            res.end();
+        }
+    })
+
+});
+
+module.exports = router;
