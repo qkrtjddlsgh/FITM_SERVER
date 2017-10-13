35f7789c426ae5cb27648b6a9b693b912423daea
park seong ho <park@parkui-MacBook-Pro.local>
2017-10-13 16:56:15 +0900

    get_member_data_stod 수정

--- routes/modify_member/get_member_key/get_member_data_stom.js
+++ routes/modify_member/get_member_key/get_member_data_stom.js
@@ -6,6 +6,7 @@ router.post('/', function(req, res){
     var recv_data = req.body;
 
     var id_email = recv_data.id_email;
+    check = 1;
 
     if(!req.body.id_email){
         var send_data = new Object();
@@ -33,32 +34,49 @@ router.post('/', function(req, res){
                     res.end();
                 }
                 else {
-                    var send_data = new Object();
+                    if(result[0].locker_finish == null || result[0].locker_start == null || result[0].start_date == null || result[0].finish_date == null){
+                        check = 0;
+                    }
 
-                    send_data.code = "2300";
-                    //send_data.response = result[0];
+                    if(check == 1) {
+                        var send_data = new Object();
 
-                    var add_data = new Object();
-                    add_data.check_register = result[0].check_register;
-                    add_data.locker_finish = result[0].locker_finish.substr(0,4) + "/" + result[0].locker_finish.substr(4,2) + "/" + result[0].locker_finish.substr(6,2);
-                    add_data.locker_start = result[0].locker_start.substr(0,4) + "/" + result[0].locker_start.substr(4,2) + "/" + result[0].locker_start.substr(6,2);
-                    add_data.locker_num = result[0].locker_num;
-                    add_data.gender = result[0].gender;
-                    add_data.birthday = result[0].birthday;
-                    add_data.doc_type = result[0].doc_type;
-                    add_data.certification = result[0].certification;
-                    add_data.remain_break_day = result[0].remain_break_day;
-                    add_data.finish_date = result[0].finish_date.substr(0,4) + "/" + result[0].finish_date.substr(4,2) + "/" + result[0].finish_date.substr(6,2);;
-                    add_data.start_date = result[0].start_date.substr(0,4) + "/" + result[0].start_date.substr(4,2) + "/" + result[0].start_date.substr(6,2);
-                    add_data.phone_number = result[0].phone_number;
-                    add_data.name = result[0].name;
-                    add_data.id_email = result[0].id_email;
-                    add_data.access_key = result[0].access_key;
+                        send_data.code = "2300";
+                        //send_data.response = result[0];
 
-                    send_data.response = add_data;
+                        var add_data = new Object();
+                        add_data.check_register = result[0].check_register;
+                        add_data.locker_finish = result[0].locker_finish.substr(0, 4) + "/" + result[0].locker_finish.substr(4, 2) + "/" + result[0].locker_finish.substr(6, 2);
+                        add_data.locker_start = result[0].locker_start.substr(0, 4) + "/" + result[0].locker_start.substr(4, 2) + "/" + result[0].locker_start.substr(6, 2);
+                        add_data.locker_num = result[0].locker_num;
+                        add_data.gender = result[0].gender;
+                        add_data.birthday = result[0].birthday;
+                        add_data.doc_type = result[0].doc_type;
+                        add_data.certification = result[0].certification;
+                        add_data.remain_break_day = result[0].remain_break_day;
+                        add_data.finish_date = result[0].finish_date.substr(0, 4) + "/" + result[0].finish_date.substr(4, 2) + "/" + result[0].finish_date.substr(6, 2);
+                        add_data.start_date = result[0].start_date.substr(0, 4) + "/" + result[0].start_date.substr(4, 2) + "/" + result[0].start_date.substr(6, 2);
+                        add_data.phone_number = result[0].phone_number;
+                        add_data.name = result[0].name;
+                        add_data.id_email = result[0].id_email;
+                        add_data.access_key = result[0].access_key;
 
-                    res.send(send_data);
-                    res.end();
+                        send_data.response = add_data;
+
+                        res.send(send_data);
+                        res.end();
+                    }
+                    else if(check == 0){
+                        var send_data = new Object();
+                        send_data.code = "1300";
+
+                        var add_data = new Object();
+                        add_data.message = "Not exist date";
+                        send_data.response = add_data;
+
+                        res.send(send_data);
+                        res.end();
+                    }
                 }
             }
         });
