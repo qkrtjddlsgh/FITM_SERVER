649a4f7e5f87d38e4f639cadb738909bedb7c17e
park seong ho <park@parkui-MacBook-Pro.local>
2017-10-17 17:21:28 +0900

    set_remain_day 수정

--- routes/modify_member/update_member_data_stod/set_remain_day.js
+++ routes/modify_member/update_member_data_stod/set_remain_day.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
 var members = require('../../../models/Member');
 var remains = require('../../../models/Remain_List');
@@ -141,6 +141,95 @@ router.post('/', function(req, res){
                         var res_data = new Object();
                         res_data.code = "9999";
                         res_data.message = "정상적으로 신청되었습니다.";
+
+                        res.send(res_data);
+                        res.end();
+                    }
+                });
+            }
+        }
+    });
+});
+
+module.exports = router;*/
+
+var express = require('express');
+var router = express.Router();
+var members = require('../../../models/Member');
+var remains = require('../../../models/Remain_List');
+
+router.post('/', function(req, res){
+    var recv_data = req.body;
+
+    var start_date = recv_data.start_date;
+    var end_date = recv_data.end_date;
+    var name = recv_data.name;
+    var id_email = recv_data.id_email;
+    var comments = recv_data.comments;
+
+    var s_year = Number(start_date.substr(0,4));
+    var s_month = Number(start_date.substr(4,2))-1;
+    var s_day = Number(start_date.substr(6,2))-1;
+
+    var e_year = Number(end_date.substr(0,4));
+    var e_month = Number(end_date.substr(4,2))-1;
+    var e_day = Number(end_date.substr(6,2))-1;
+
+    var start = new Date(s_year, s_month, s_day);
+    var end = new Date(e_year, e_month, e_day);
+
+    var diff = end - start;
+
+    members.find({id_email: id_email, certification: 2}, function(err, doc){
+        if(err){
+            console.error(err.message);
+        }
+        if(doc.length == 0){
+            var res_data = new Object();
+            res_data.code = "7777";
+            res_data.message = "id_email이 존재하지 않습니다. or 정회원이 아닙니다.";
+
+            res.send(res_data);
+            res.end();
+        }
+        else{
+            if(diff > doc[0].remain_break_day*86400000){
+                var res_data = new Object();
+                res_data.code = "8888";
+                res_data.message = "잔여일수가 부족합니다.";
+
+                res.send(res_data);
+                res.end();
+            }
+            else{
+                remains.find({id_email: id_email, state: 0}, function(err, result){
+                    if(err){
+                        console.error(err.message);
+                    }
+                    if(result.length == 0){
+                        var new_remains = new remains();
+                        new_remains.start_date = start_date;
+                        new_remains.end_date = end_date;
+                        new_remains.name = name;
+                        new_remains.id_email = id_email;
+                        new_remains.comments = comments;
+
+                        new_remains.state = 0;
+                        new_remains.diff = diff;
+                        new_remains.message = "신청중입니다.";
+                        new_remains.save();
+
+                        var res_data = new Object();
+                        res_data.code = "9999";
+                        res_data.message = "정상적으로 신청되었습니다.";
+
+                        res.send(res_data);
+                        res.end();
+                    }
+                    else{
+                        var res_data = new Object();
+                        res_data.code = "6666";
+                        res_data.message = "이미 신청되었습니다.";
 
                         res.send(res_data);
                         res.end();
