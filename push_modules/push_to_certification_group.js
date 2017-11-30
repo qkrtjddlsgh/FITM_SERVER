a0404154fafe19b80c75fa9827d5169eb0f05694
Park-Ryeong-Min <somuch0321@gmail.com>
2017-11-30 16:33:01 +0900

    push 그룹 다이나믹하게 전송하도록 추가 구현

--- push_modules/push_to_certification_group.js
+++ push_modules/push_to_certification_group.js
@@ -7,6 +7,7 @@ var express = require('express');
 var router = express.Router();
 var member = require('../models/Member');
 
+/*
 router.post('/', function (req, res) {
     var recv_data = req.body;
     var certification = recv_data.certification;
@@ -65,6 +66,68 @@ router.post('/', function (req, res) {
         }
     });
 
+});
+*/
+
+router.post('/', function (req, res) {
+    var recv_data = req.body;
+
+    var certification = recv_data.certification; // list
+
+    var push_obj = {
+        title : recv_data.title,
+        body : recv_data.body,
+        notification_type : 5
+    }
+
+    member.find({doc_type : "member_data"}, function (err, result) {
+        if(err){
+            console.error(JSON.stringify(err));
+            var send_obj = new Object();
+            send_obj.code = "5300";
+            send_obj.response = { message : "database error" };
+            res.send(send_obj);
+            res.end();
+        }else{
+            if(result.length == 0){
+                var send_obj = new Object();
+                send_obj.code = "9999";
+                send_obj.response = { message : "param error" };
+                res.send(send_obj);
+                res.end();
+            }else{
+                var success_cnt = 0;
+                var fail_cnt = 0;
+                for (var i = 0; i < result.length; i++) {
+                    if(certification[result[i].certification] == 0){
+                        continue;
+                    }
+                    var message = {
+                        to : result[i].device_token,
+                        priority : 'high',
+                        data : push_obj,
+                        notification : push_obj
+                    }
+                    fcm.send(message, function (err, response) {
+                        if(err){
+                            fail_cnt += 1;
+                            console.error(JSON.stringify(err));
+                        }
+                        else{
+                            success_cnt += 1;
+                            //console.log("push success");
+                        }
+                    });
+                }
+                var send_obj = new Object();
+                console.log("success : " + success_cnt + " , fail : " + fail_cnt );
+                send_obj.code = "2222";
+                send_obj.response = { push_count : result.length };
+                res.send(send_obj);
+                res.end();
+            }
+        }
+    })
 });
 
 module.exports = router;
