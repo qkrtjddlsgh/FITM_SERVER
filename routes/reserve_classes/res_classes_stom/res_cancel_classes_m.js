504873e4bb2bf85dae21454f68e23cd1d7f2f863
Park-Ryeong-Min <somuch0321@gmail.com>
2017-12-18 16:26:42 +0900

    로그로 디버깅 중 7

--- routes/reserve_classes/res_classes_stom/res_cancel_classes_m.js
+++ routes/reserve_classes/res_classes_stom/res_cancel_classes_m.js
@@ -58,7 +58,7 @@ router.post('/', function(req, res){
                         };
                         
                         time_table.find({
-                            'classes.$.participants' : {
+                            'classes.$.participant' : {
                                 $elemMatch : {
                                     "access_key": access_key
                                 }
