fa965b9b06d9494078243657c1a886f792eff04d
Park-Ryeong-Min <somuch0321@gmail.com>
2017-12-18 17:31:06 +0900

    로그로 디버깅 중 13 + 쿼리 수정

--- routes/reserve_classes/res_classes_stom/res_cancel_classes_m.js
+++ routes/reserve_classes/res_classes_stom/res_cancel_classes_m.js
@@ -46,12 +46,12 @@ router.post('/', function(req, res){
 
                         var query = {
                             $pull: {
-                                "classes.participant.access_key": access_key
+                                "classes.$.participant.$.access_key": access_key
                             }
                         };
                         
                         time_table.find({
-                            'classes.participant.access_key' : access_key
+                            'classes.$.participant.$.access_key' : access_key
                         },function (err, result) {
                             if(err){
                                 console.error(err);
@@ -61,7 +61,7 @@ router.post('/', function(req, res){
                         });
 
                         time_table.update({
-                            'classes.participant.access_key' : access_key
+                            'classes.$.participant.$.access_key' : access_key
                         }, query, function (err, result) {
                             if (err) {
                                 console.error(err.message);
